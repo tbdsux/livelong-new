311d35e1dd6f4a27304516611967bbfd96495216
TheBoringDude <iamcoderx@gmail.com>
2021-05-27 11:26:17 +0800

    index page - add contact us section - feedbacks section

---
+++ components/index/feedback.vue
@@ -0,0 +1,91 @@
+<template>
+  <container width="w-4/5">
+    <div class="py-14 text-center">
+      <h3 class="text-4xl font-extrabold text-green-500 leading-relaxed">
+        What our amazing customers say
+      </h3>
+      <p class="text-lg text-gray-500">
+        We are very thankful for overyone who sent feedbacks and messages to us
+      </p>
+
+      <div class="mt-8 grid grid-cols-2 gap-4">
+        <div
+          v-for="(feedback, index) in feedbacks"
+          :key="index"
+          class="
+            p-4
+            rounded-lg
+            border border-green-300
+            flex flex-col
+            items-center
+            justify-center
+          "
+        >
+          <q class="text-gray-600 tracking-wide">{{ feedback.comment }}</q>
+          <p class="font-bold tracking-wide text-green-500">
+            - {{ feedback.author }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      feedbacks: [
+        {
+          comment:
+            "Lovelovelove! Nice to find one around.... With the busy schedule at work, it's great to find a shop that does home food for the detox freak!!! Thank...",
+          author: 'Michelinchef Tori Eres S.',
+        },
+        {
+          comment:
+            "Everything is Perfect and Healthy.. Perfectly healthy .. Will be goin' back for more.. Thanks for having us at closing time.",
+          author: 'Ena De Jesus A.',
+        },
+        {
+          comment: 'Eating is no longer sinful! Love love love! Nom nom nom!',
+          author: 'Dwight Mijares L.',
+        },
+        {
+          comment:
+            'Dito ang malupit na detox! Tried "the hulk" and "ironman", ay grabe ang lupet!',
+          author: ' Paul Albert G.',
+        },
+        {
+          comment:
+            'The first detox organic juice bar in baguio if not the philippines....',
+          author: 'Tiam D.',
+        },
+        {
+          comment:
+            'Amazing unique and all healthful food ! Nothing beats this amazing restaurant ! Fall I n love with the food , the ambiance and the staff !',
+          author: 'Solomon V.',
+        },
+        {
+          comment:
+            '... Super nakaka busog naka isa palang ako and saktong sakto, and saktong sakto, hindi nakak umay, Super sulit ang price... Kaka kain ko Lasagna, supper winner!, parang hindi low carb. Will definitely order again and try the others...',
+          author: 'Customer',
+        },
+        {
+          comment:
+            '...Sarap nung spring roll. Nagustuhan ni husband yung red velvet pandesal. Super soft',
+          author: 'Customer',
+        },
+        {
+          comment:
+            '... I am currently drinking the collagen milk tea. super sarap',
+          author: 'Customer',
+        },
+        {
+          comment: 'Masarap po cia (Bona Vita) at nakakablooming ng skin. Thnx',
+          author: 'Customer',
+        },
+      ],
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <main>
     <!-- showcase header -->
     <container width="w-5/6">
-      <div class="pt-12 pb-16 grid grid-cols-2 gap-8 items-center">
+      <div class="pt-16 pb-24 grid grid-cols-2 gap-8 items-center">
         <div class="">
           <h3
             class="
@@ -83,13 +83,104 @@
                   d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
                 />
               </svg>
-              Order Now</a
-            >
+              Order Now
+            </a>
           </div>
         </div>
         <img src="~/assets/images/cover.jpg" class="h-72 rounded-lg ml-auto" />
       </div>
     </container>
+    <hr />
+
+    <container width="w-5/6">
+      <div class="py-16 text-center">
+        <h3 class="text-4xl font-extrabold text-green-500 leading-relaxed">
+          Where are we?
+        </h3>
+        <p class="text-xl text-gray-500">
+          You can visit us with the address below or you could also contact us
+          with our contact number.
+        </p>
+
+        <div class="mt-8 grid grid-cols-2 items-center text-left">
+          <div class="w-full">
+            <iframe
+              class="w-full h-72 rounded-lg"
+              src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d3827.2522326005533!2d120.60569237817027!3d16.412011298017436!3m2!1i1024!2i768!4f13.1!4m8!3e0!4m0!4m5!1s0x3391a1505a2f2aeb%3A0xdca373e1272bff1f!2sLeonard%20Wood%20Rd%2C%20Baguio%2C%20Benguet!3m2!1d16.413235999999998!2d120.6070718!5e0!3m2!1sen!2sph!4v1622083552144!5m2!1sen!2sph"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+          <div class="ml-12">
+            <ul>
+              <li class="my-3 p-3 border-t border-b">
+                <h4
+                  class="
+                    text-xl
+                    font-extrabold
+                    inline-flex
+                    items-center
+                    text-green-500
+                  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 mr-1"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  Our Address
+                </h4>
+                <p class="text-2xl text-gray-500 tracking-tight font-light">
+                  Siapno Road, Pacdal, <br />
+                  Baguio City 2600, Philippines
+                </p>
+              </li>
+              <li class="my-3 p-3 border-t border-b">
+                <h4
+                  class="
+                    text-xl
+                    font-extrabold
+                    inline-flex
+                    items-center
+                    text-green-500
+                  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  Mobile Number
+                </h4>
+                <p class="text-2xl text-gray-500 tracking-tight font-light">
+                  09954179115
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </container>
+
+    <hr />
+
+    <IndexFeedback />
+
     <hr />
   </main>
 </template>
